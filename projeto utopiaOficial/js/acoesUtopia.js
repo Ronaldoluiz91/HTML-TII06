865d3855e6f65fcf537fa55d4b83340728cf1402
Ronaldoluiz91 <ronaldo.lsilva6@senacsp.edu.br>
2024-03-15 17:28:08 -0300

    projeto utopia

---
+++ projeto utopiaOficial/js/acoesUtopia.js
@@ -0,0 +1,10 @@
+alert('funcionou')
+
+var menu = document.getElementById('iconMenu'); //botão do menu mobile (mostrar e ocultar)
+
+
+menu.onclick = function(){
+document.getElementById('itens').style.display = 'none';
+}
+
+
